e72f726c95430dd5ef9f76dcefa6dd50beec353d
Abhinav Mishra <abhinavmishra3322@gmail.com>
2024-05-30 23:06:30 +0530

    zs

--- defs.h
+++ defs.h
@@ -1,10 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+static int scanint(int c);
+static int chrpos(char *s, int c);
+
 struct token {
     int token;
     int intvalue;
 };
 
 enum {
-    T_PLUS, T_MINUS, T_STAR, T_SLASH, T_INTIT
+    T_PLUS, T_MINUS, T_STAR, T_SLASH, T_INTLIT
 };
 //T_INTIT will store value of any integer
 
--- scan.c
+++ scan.c
@@ -1,5 +1,11 @@
 #include "data.h"
 #include "defs.h"
+#include "decl.h"
+
+
+static int scanint(int c);
+static int chrpos(char *s, int c);
+
 
 static int next(void)
 {
@@ -32,4 +38,56 @@ static int skip(void)
     return c;
 }
 
-int scan(struct)
+int scan(struct token *t){
+    int c;
+
+    c = skip();
+
+    switch(c){
+    case EOF:
+        return (0);
+    case '+':
+        t -> token = T_PLUS;
+        break;
+    case '-':
+        t -> token = T_MINUS;
+        break;
+    case '*':
+        t -> token = T_STAR;
+        break;
+    case '/':
+        t -> token = T_SLASH;
+        break;
+    default:
+        if(isdigit(c)){
+            t->intvalue = scanint(c);
+            t->token = T_INTLIT;
+            break;
+        }
+        printf("Unrecognised character %c on line %d\n", c, Line);
+        exit(1);
+    }
+
+    return (1); // we found a token
+
+};
+
+
+static int scanint(int c){
+    int k, val = 0;
+
+    while((k = chrpos("0123456789", c)) >=0){
+        val = val*10 +k;
+        c = next();
+    }
+
+    putback(c);
+    return val;
+}
+
+static int chrpos(char *s, int c){
+    char *p;
+
+    p = strchr(s, c);
+    return (p ? p -s :-1);
+}
