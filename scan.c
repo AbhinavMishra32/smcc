a8ae814b9d8dfbf22b0316a362ccafe87cdb9b18
rapidtyper <abhinavmishra3322@gmail.com>
2024-05-30 21:38:19 +0530

    3

--- scan.c
+++ scan.c
@@ -1,10 +1,12 @@
 #include "data.h"
 #include "defs.h"
 
-static int next(void){
+static int next(void)
+{
     int c;
 
-    if (Putback){
+    if (Putback)
+    {
         c = Putback;
         Putback = 0;
         return c;
@@ -13,6 +15,21 @@ static int next(void){
     c = fgetc(Infile);
 }
 
-static void putback(int c){
+static void putback(int c)
+{
     Putback = c;
 }
+
+static int skip(void)
+{
+    int c;
+
+    c = next();
+    while (' ' == c || '\t' == c || '\n' == c || '\r' == c || '\f' == c)
+    {
+        c = next();
+    }
+    return c;
+}
+
+int scan(struct)
