866ef4ee19d3319fec7c922d9e127b028c5dd8b1
rapidtyper <abhinavmishra3322@gmail.com>
2024-06-03 22:43:03 +0530

    code gen for x86 (2)

--- cg.c
+++ cg.c
@@ -16,5 +16,38 @@ int cgload(int value)
 
 int cgadd(int r1, int r2)
 {
-    fprintf(Outfile, "\taddq\t%s, %s\n")
+    fprintf(Outfile, "\taddq\t%s, %s\n", reglist[r1], reglist[r2]);
+    free_register(r1);
+    return r2;
+}
+
+int cgmul(int r1, int r2)
+{
+    fprintf(Outfile, "\timulq\t%s, %s\n", reglist[r1], reglist[r2]);
+    free_register(r1);
+    return r2;
+}
+
+int cgsub(int r1, int r2)
+{
+    fprintf(Outfile, "\tsubq\t%s, %s\n", reglist[r2], reglist[r1]);
+    free_register(r2);
+    return r1;
+}
+
+int cgdiv(int r1, int r2)
+{
+    fprintf(Outfile, "\tmovq\t%s, %%rax\n", reglist[r1]);
+    fprintf(Outfile, "\tcqo\n");
+    fprintf(Outfile, "\tidivq\t%s\n", reglist[r2]);
+    fprintf(Outfile, "\tmovq\t%%rax, %s\n", reglist[r1]);
+    free_register(r2);
+    return r1;
+}
+
+void cgprintint(int r)
+{
+    fprintf(Outfile, "\tmovq\t%s, %%rdi\n", reglist[r]);
+    fprintf(Outfile, "\tcall\tprintint\n");
+    free_register(r);
 }
