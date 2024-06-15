e0523f8a19a1f3ae47203d8b6181d86caa4ea7fc
rapidtyper <abhinavmishra3322@gmail.com>
2024-06-15 22:51:33 +0530

    Refactor cg.c for improved code generation and portability

--- out.s
+++ out.s
@@ -1,38 +1,33 @@
-    .text
+.section .rodata
 .LC0:
-    .asciz  "%d\n"
-printint:
-    STP X29, X30, [SP, #-16]!
-    MOV X29, SP
-    SUB SP, SP, #16
-    STR W0, [SP, #12]
-    ADR X0, .LC0
-    LDR X0, [X0]
-    ADD X1, SP, #12
-    MOV X2, X0
-    BL printf
-    LDP X29, X30, [SP], #16
-    RET
+    .string "%d\n"
 
-    .globl main
+.text
+.globl printint
+printint:
+    pushq %rbp
+    movq %rsp, %rbp
+    subq $16, %rsp
+    movl %edi, -4(%rbp)
+    movl -4(%rbp), %eax
+    movl %eax, %esi
+    leaq .LC0(%rip), %rdi
+    movl $0, %eax
+    call printf
+    nop
+    leave
+    ret
+
+.globl main
 main:
-    STP X29, X30, [SP, #-16]!
-    MOV X29, SP
-    SUB SP, SP, #16
-    MOV W8, #2
-    MOV W9, #3
-    MOV W10, #5
-    MUL W11, W10, W9
-    ADD W11, W11, W8
-    MOV W8, #8
-    MOV W9, #3
-    MOV W0, W8
-    MOV W1, W9
-    BL __aeabi_idivmod
-    MOV W8, W0
-    SUB W11, W11, W8
-    MOV W0, W11
-    BL printint
-    MOV W0, #0
-    LDP X29, X30, [SP], #16
-    RET
+    pushq %rbp
+    movq %rsp, %rbp
+
+    # Test with a known value
+    movq $42, %rdi  # Load 42 into rdi (argument for printint)
+    call printint   # Call printint to print 42
+
+    # Exit
+    movl $0, %eax
+    popq %rbp
+    ret
