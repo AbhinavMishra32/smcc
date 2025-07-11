a700356ff33d7e1c0cab8e52c51da0384d58d5f3
rapidtyper <abhinavmishra3322@gmail.com>
2025-07-11 22:39:44 +0530

    Word reading working

--- smcc2/src/lexer.c
+++ smcc2/src/lexer.c
@@ -3,15 +3,13 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <malloc.h>
 
 
 char* read_word(const char* input, int* index) {
     char c = input[*index];
 
-    printf("input: %s", input);
-    printf("C: %c\n", c);
-
-    static char word[256];
+    char* word = malloc(256 * sizeof(char));
     int i = 0;
 
     while (isalpha(c) && i < 255) {
@@ -20,16 +18,23 @@ char* read_word(const char* input, int* index) {
         c = input[*index];
         i++;
     }
-    word[i] ='\0';
+    word[i] = '\0';
 
-    printf("Reading: %s", word);
     return word;
 }
 
 Token next_token(const char* input, int* index);
 Token next_token(const char* input, int* index)
 {
-    char* word = read_word(input, index);
+    char c = input[*index];
+    while(c != '\0'){
+        char* word = read_word(input, index);
+        printf("Word: %s\n", word);
+        c = input[*index];
+        if (c == ' ' || c == '\n'){
+            (*index)++;
+        }
+    }
 
     Token token = { TOKEN_EO, NULL };
     return token;
