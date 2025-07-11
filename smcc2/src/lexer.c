27feec842a8ead91f6690eb6e97d4c7bedb8acb9
rapidtyper <abhinavmishra3322@gmail.com>
2025-07-11 20:36:41 +0530

    Makefile and lexer

--- smcc2/src/lexer.c
+++ smcc2/src/lexer.c
@@ -1,16 +1,24 @@
 #include "defs.h"
 #include "decl.h"
 #include <stdio.h>
+#include <ctype.h>
 
 Token next_token(const char *input, int *index)
 {
     char c = input[*index];
     printf("This is c: %c", c);
 
-    // while()
-    
-    // Token token = {TOKEN_EO, NULL};
-    // return token;
+
+    if (isalpha(c)){
+
+    }
+
+    Token token = {TOKEN_EO, NULL};
+    return token;
+}
+
+char* word(char *input, int *index){
+    if ()
 }
 
 // TokenType keyword_or_ident(char *word) {
