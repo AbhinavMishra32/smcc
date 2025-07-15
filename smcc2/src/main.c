0ce53d0a494f438ceace48089e09ebab3bf497d5
rapidtyper <abhinavmishra3322@gmail.com>
2025-07-15 13:27:55 +0530

    Lexer improvements

--- smcc2/src/main.c
+++ smcc2/src/main.c
@@ -2,7 +2,8 @@
 
 int main(){
     int pos = 0;
-    char sentence[] = "int return 1235       asdfasdf { }";
+    char sentence[] = "int return 1235 asdfasdf  ++- --   { }  jasdf";
+                    // 01234567891011
     Token* t = token_list(sentence, &pos);
 
     return 0;
